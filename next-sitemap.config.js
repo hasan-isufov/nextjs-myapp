497be66c9c1becea4bc840660848a74f72841ed0
hasan isufov <hsn_90@outlook.com.tr>
2025-06-01 20:27:58 +0100

    Google search

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.zenith-web.co.uk/', // kendi domainin
+  generateRobotsTxt: true,              // robots.txt de otomatik oluşur
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000,
+}
